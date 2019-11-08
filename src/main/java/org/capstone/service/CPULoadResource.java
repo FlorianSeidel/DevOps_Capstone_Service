5aeeb31e3c10feb7a68b87a31dab2565a1cf5ce5
Florian Seidel <seidel.florian@gmail.com>
2019-11-08 18:28:23 +0100

    Better load creation using FakeLoad library.

--- src/main/java/org/capstone/service/CPULoadResource.java
+++ src/main/java/org/capstone/service/CPULoadResource.java
@@ -1,5 +1,7 @@
 package org.capstone.service;
 
+import com.martensigwart.fakeload.*;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -16,22 +18,13 @@ public class CPULoadResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String createLoad() {
-        List<Future<?>> futures = new LinkedList<Future<?>>();
-        for(int k=0;k<4;k++) {
-            futures.add(executor.submit(() -> {
-                double j = 0;
-                for (int i = 0; i < 100000; i++) {
-                    j += Math.sqrt(i);
-                }
-            }));
-        }
-        futures.forEach(future -> {
-            try {
-                future.get();
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-            }
-        });
+        FakeLoad fakeload = FakeLoads.create()
+                .lasting(5, TimeUnit.SECONDS)
+                .withCpu(80)
+                .withMemory(50, MemoryUnit.MB);
+
+        FakeLoadExecutor executor = FakeLoadExecutors.newDefaultExecutor();
+        executor.execute(fakeload);
         return "Done";
     }
 
